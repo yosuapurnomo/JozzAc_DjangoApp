668923f96ece1a79d5d28c1f352de0c66b6bdad7
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-07-28 13:45:46 +0700

    'static'

--- jozzAC/jozzAc/urls.py
+++ jozzAC/jozzAc/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include, re_path
 
@@ -19,7 +20,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += sstaticfiles_urlpatterns()
 
 handler404 = 'jozzAc.views.handle404'
 
