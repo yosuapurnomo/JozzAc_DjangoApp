e47f81e801abb934e75a08c4202d05f920de2f56
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-07-28 13:01:10 +0700

    'static'

--- jozzAC/jozzAc/settings.py
+++ jozzAC/jozzAc/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/root/JozzAc/jozzAC/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = '/root/JozzAc/jozzAC/staticfiles/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
