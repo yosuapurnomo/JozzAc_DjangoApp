771fb0db2488292b55683f07f5615227da82294c
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-07-30 00:55:45 +0700

    'prod_dev'

---
+++ etc/gunicorn.conf.py
@@ -0,0 +1,4 @@
+workers = 2
+errorlog = '/root/demo/JozzAC/jozzAc.gunicorn.error'
+accesslog = '/root/demo/JozzAC/jozzAc.gunicorn.access'
+loglevel = 'debug'
--- jozzAC/jozzAc/prod.py
+++ jozzAC/jozzAc/prod.py
@@ -1,3 +1,8 @@
 from .settings import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = [
+	'12.0.0.1', '101.50.2.224', 'jozacsurabaya.com', 'jozacsurabaya',
+	'101.50.2.224:8000',
+]
