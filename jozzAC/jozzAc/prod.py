1cd90e5a7ee5ace9adea9825f0657bcae067ca34
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-07-31 02:26:58 +0700

    'nginx_service'

--- jozzAC/jozzAc/prod.py
+++ jozzAC/jozzAc/prod.py
@@ -8,7 +8,8 @@ STATIC_ROOT = '/root/demo/www/public'
 
 ALLOWED_HOSTS = [
 	'jozacsurabaya.com', 'jozacsurabaya',
-	'jozacsurabaya.com:9001', '101.50.2.224:9000'
+	'jozacsurabaya.com:9001', '101.50.2.224:9001',
+	'127.0.0.1:9001'
 ]
 
 # '101.50.2.224:9000'
