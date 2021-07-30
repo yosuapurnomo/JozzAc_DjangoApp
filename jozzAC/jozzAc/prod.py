806b76ddd0ab7fb24cd5e88a2f63672c3d7211cd
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-07-31 02:50:35 +0700

    'nginx_service'

--- jozzAC/jozzAc/prod.py
+++ jozzAC/jozzAc/prod.py
@@ -2,14 +2,16 @@ from .settings import *
 
 DEBUG = False
 
-STATIC_URL = '/root/demo/www/public/'
+STATIC_URL = '/staitc/'
 
-STATIC_ROOT = '/root/demo/www/public'
+
 
 ALLOWED_HOSTS = [
 	'jozacsurabaya.com', 'jozacsurabaya',
 	'jozacsurabaya.com:9001', '101.50.2.224:9001',
 	'127.0.0.1:9001'
 ]
+
+STATIC_ROOT = '/root/demo/www/public'
 
 # '101.50.2.224:9000'
