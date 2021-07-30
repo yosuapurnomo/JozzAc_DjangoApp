2c83c545da704d456db1407107ea0adfd2489812
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-07-31 03:09:23 +0700

    'nginx_service'

--- jozzAC/jozzAc/prod.py
+++ jozzAC/jozzAc/prod.py
@@ -2,7 +2,7 @@ from .settings import *
 
 DEBUG = False
 
-# STATIC_URL = '/staitc/'
+STATIC_URL = '/prefix/'
 
 
 ALLOWED_HOSTS = [
@@ -11,6 +11,6 @@ ALLOWED_HOSTS = [
 	'127.0.0.1:9001', '*'
 ]
 
-STATIC_ROOT = '/root/demo/www/public'
+STATIC_ROOT = '/root/demo/www/public/'
 
 # '101.50.2.224:9000'
--- jozzAC/jozzAc/settings.py
+++ jozzAC/jozzAc/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = '/root/demo/www/public'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
