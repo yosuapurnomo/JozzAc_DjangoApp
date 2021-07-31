9aa03fb5f47e2fbd70d22bca35f4711088a459e7
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-08-01 03:45:28 +0700

    'uwsgi'

--- jozzAC/jozzAc/settings.py
+++ jozzAC/jozzAc/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = '/root/demo/www/public/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
