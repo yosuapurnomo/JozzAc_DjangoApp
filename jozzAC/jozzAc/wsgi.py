c9e29f4474b9acdad460bd324150658969562584
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-08-01 02:09:26 +0700

    'uwsgi'

--- jozzAC/jozzAc/wsgi.py
+++ jozzAC/jozzAc/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jozzAc.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jozzAc.settings')
 
 application = get_wsgi_application()
