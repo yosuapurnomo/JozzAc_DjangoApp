b20f002d115f45ef67b80f0d3732208a7752e769
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-08-01 02:14:05 +0700

    'uwsgi'

--- jozzAC/jozzAc/dev.py
+++ jozzAC/jozzAc/dev.py
@@ -3,8 +3,11 @@ from .settings import *
 DEBUG = True
 
 ALLOWED_HOSTS = [
-	'www.jozacsurabaya.com', 'jozacsurabaya.com', 'jozacsurabaya'
+	'www.jozacsurabaya.com', 'jozacsurabaya.com',
+	 'jozacsurabaya', '*'
 ]
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = '/root/demo/www/public/static'
+MEDIA_ROOT = '/root/demo/www/public/media'
