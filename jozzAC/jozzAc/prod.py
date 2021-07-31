a3ec7c71c62498894c44caf5e07d843cf9572272
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-08-01 01:05:09 +0700

    'static'

--- jozzAC/jozzAc/prod.py
+++ jozzAC/jozzAc/prod.py
@@ -2,14 +2,13 @@ from .settings import *
 
 DEBUG = False
 
-STATIC_URL = '/static/'
-
 
 ALLOWED_HOSTS = [
-	'*'
+	'www.jozacsurabaya.com', 'jozacsurabaya.com', 'jozacsurabaya'
 ]
 
-STATIC_ROOT = '/root/demo/jozzAC/static/'
+STATIC_ROOT = '/root/demo/www/public/static'
+MEDIA_ROOT = '/root/demo/www/public/media'
 
 # '101.50.2.224:9000'
 # 'jozacsurabaya.com', 'jozacsurabaya',
