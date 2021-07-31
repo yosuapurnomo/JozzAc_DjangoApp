d9aa4762a54397eb7bc55d03d2673c01eaceea51
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-07-31 14:28:56 +0700

    'dev'

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -1,6 +1,10 @@
 workers = 3
+syslog = True
+umask = 0
 errorlog = '/root/demo/var/logs/jozzAc.gunicorn.error'
 accesslog = '/root/demo/var/logs/jozzAc.gunicorn.access'
-loglevel = 'debug'
+loglevel = "info"
+user="root"
+group="root"
 
 bind = ['101.50.2.224:9001']
