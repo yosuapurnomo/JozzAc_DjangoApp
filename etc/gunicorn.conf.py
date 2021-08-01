3d155199374555a64f78caa65b720d424a5c7b2c
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-08-01 23:21:09 +0700

    update home page

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -3,4 +3,4 @@ errorlog = '/home/demo/var/logs/jozzAc.gunicorn.error'
 accesslog = '/home/demo/var/logs/jozzAc.gunicorn.access'
 loglevel = "info"
 
-bind = ['101.50.2.224:8000']
+bind = ['101.50.2.224:80']
